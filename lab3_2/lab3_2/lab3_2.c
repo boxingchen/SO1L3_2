cedf16ed844219d87519b3e26989049be9e67bac
boxingchen <boxingchen@users.noreply.github.com>
2014-11-03 15:18:06 +0100

    Submitted code

--- lab3_2/lab3_2/lab3_2.c
+++ lab3_2/lab3_2/lab3_2.c
@@ -6,7 +6,53 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define M_PI 3.14159265358979323846								// pi constant
+double arctan_IE1(double);										// function prototypes
+double arccot_IE1(double);
+double errorAtan(double);
+double errorCot(double);
+
+int main()
+{
+	double dvalue;												// arctan argument
+	int i = 0;													// cycle iterator
+	double result = 0;											// arctan computation result
+	double summant;												// sequence summant
+	double threshold = 1e-9;									// precision threshold
+	double pi = 4*atan(1.0);									// get the PI value
+
+
+	printf("Enter arctan() argument: ");
+	fflush(stdin);
+	scanf("%lf", &dvalue);
+	if(fabs(dvalue) >= 1)
+	{
+		printf("Sorry, this program only works for arguments of arctan with absolute value less than 1\n");
+		system("pause");
+		return 0;
+	}
+
+	do
+	{
+		printf("Iteration %i\r", i);							// just for convinience
+		if(i%2)													// I used IF instead of computing powers of -1
+			summant = -1 * pow(dvalue, 2*i + 1)/(2*i + 1);
+		else
+			summant = pow(dvalue, 2*i + 1)/(2*i + 1);
+		result += summant;
+		i++;
+	}
+	while(fabs(summant) >= threshold);							// continue the cycle as long as the last summant contribution is more than threshold
+	system("mode CON: COLS=95");								// enlarge the console window so my beautiful table fits
+	printf("\n |------------------------------------------------------------------------------------------|");
+	printf("\n |  arctan_IE1  |  arccot_IE1  |     atan     |     acot     |   error atan  |  error acot  |");
+	printf("\n |------------------------------------------------------------------------------------------|");
+	printf("\n | %12.10lf | %12.10lf | %12.10lf | %12.10lf | %12.10lf | %12.10lf |", arctan_IE1(dvalue), arccot_IE1(dvalue), atan(dvalue), pi/2 - atan(dvalue), errorAtan(dvalue), errorCot(dvalue));
+	printf("\n |__________________________________________________________________________________________|\n\n");
+	printf("  ");
+
+	system("pause");
+	return 0;
+}
 
 double arctan_IE1(double x)
 {
@@ -16,48 +62,36 @@ double arctan_IE1(double x)
 	double threshold = 1e-9;									// precision threshold
 	do
 	{
-		if(i%2)
+		if(i%2)													// I used IF instead of computing powers of -1
 			summant = -1 * pow(x, 2*i + 1)/(2*i + 1);
 		else
 			summant = pow(x, 2*i + 1)/(2*i + 1);
 		result += summant;
 		i++;
 	}
-	while(fabs(summant) >= threshold);
+	while(fabs(summant) >= threshold);							// continue the cycle as long as the last summant contribution is more than threshold
 	return result;
 }
 
-double arccot_IE1(double x)
+double arccot_IE1(double x)										// since the mathematical functions are related, I make use of the previously written function
 {
-	return (M_PI/2 - arctan_IE1(x));
+	double pi = 4*atan(1.0);
+	return (pi/2 - arctan_IE1(x));
 }
 
-int main()
+double errorAtan(double value)									// this function returns relative error compared to library arcus tangent function
 {
-	double dvalue;												// arctan argument
-	int i = 0;													// cycle iterator
-	double result = 0;											// arctan computation result
-	double summant;												// sequence summant
-	double threshold = 1e-9;									// precision threshold
+	if(value)													// check for 0 input so we don't divide by 0
+		return (arctan_IE1(value) - atan(value))/atan(value);
+	else
+		return 0;
+}
 
-	printf("Enter arctan() argument: ");
-	fflush(stdin);
-	scanf("%lf", &dvalue);
-	do
-	{
-		printf("Iteration %i\r", i);
-		if(i%2)
-			summant = -1 * pow(dvalue, 2*i + 1)/(2*i + 1);
-		else
-			summant = pow(dvalue, 2*i + 1)/(2*i + 1);
-		result += summant;
-		i++;
-	}
-	while(fabs(summant) >= threshold);
-	printf("\narctan(%.3lf) computed with precision up to %.1le equals %lf rad\n", dvalue, threshold, result);
-	printf("Function arctan_IE1 returned %lf rad\n", arctan_IE1(dvalue));
-	printf("Function arccot_IE1 returned %lf rad\n", arccot_IE1(dvalue));
-
-	system("pause");
-	return 0;
+double errorCot(double value)									// this function returns relative error compared to library arcus cotangent function
+{
+	double pi = 4*atan(1.0);
+	if(value)
+		return (arccot_IE1(value) - (pi/2 - atan(value)))/(pi/2 - atan(value));
+	else
+		return pi/2;
 }
